008036eb70dea7eb7ab6ab9957f323cd0c7f0f5b
生貝健人 <rayna@pckntrayna.local>
2019-06-13 14:48:12 +0900

    カテゴリ機能palceの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_place, only: [:new, :edit]
 
   # GET /posts
   # GET /posts.json
@@ -69,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :auther, images:[])
+      params.require(:post).permit(:title, :content, :auther, :place, images:[])
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_142808) do
+ActiveRecord::Schema.define(version: 2019_06_13_042223) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,12 +42,19 @@ ActiveRecord::Schema.define(version: 2019_06_10_142808) do
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.string "auther"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "place"
   end
 
 end
