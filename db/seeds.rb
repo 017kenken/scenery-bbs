a9cab6934cc66ac1d4b5b519ead9cc8554245032
生貝健人 <rayna@pckntrayna.local>
2019-06-13 15:11:22 +0900

    placeのデータ追加とplaceのリンク削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+
+Place.create(name: '海')
+Place.create(name: '川')
+Place.create(name: '山')
+Place.create(name: '空')
+Place.create(name: '夜景')
+Place.create(name: 'その他')
