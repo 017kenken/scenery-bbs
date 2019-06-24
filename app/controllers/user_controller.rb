e8cc09720ac8e0ff61ebcb7a6e04909840eeb662
生貝健人 <rayna@pckntrayna.local>
2019-06-24 11:31:28 +0900

    フォロー機能の追加

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,7 @@
 class UserController < ApplicationController
+  before_action :set_user, only: [:show, :follow, :followers, :followees]
+
   def show
-    @user = User.find(params[:id])
     @posts = @user.posts.where(params[:user_id])
   end
 
@@ -18,12 +19,40 @@ class UserController < ApplicationController
 
   def mypage
     @user = current_user
+    @likes = @user.likees(Post)
     @posts = current_user.posts.where(params[:user_id])
+    @followees = @user.followees(User)
+    @followers = @user.followers(User)
+  end
+
+  def follow
+    current_user.toggle_follow!(@user)
+    if current_user.follows?(@user)
+      redirect_back(fallback_location: root_path, notice: "フォローしました")
+    else
+      redirect_back(fallback_location: root_path, notice: "フォローを解除しました")
+    end
+  end
+
+  def followees
+    @followees = @user.followees(User)
+
+    render :followees
+  end
+
+  def followers
+    @followers = @user.followers(User)
+
+    render :followers
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :profile)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   get 'user/:id', to:'user#show', as: 'users'
   patch 'user', to:'user#update'
   get 'users/edit', to:'user#edit'
-  get 'mypage' , to: 'user#mypage'
+  get 'mypage', to: 'user#mypage'
+  get 'follow/:id', to: 'user#followees', as: 'followees'
+  get 'follower/:id', to: 'user#followers', as: 'followers'
 
   get 'post/:id/likes', to: 'posts#likes', as: :likes
   
+  resources :user do
+    post :follow, on: :member
+  end
+
   devise_for :users
   resources :places
   root 'posts#index'
